51c0303de2454c598d57c26de6a74fd553aab595
jyoti <jyoti.zanwar@siemens.com>
2022-10-07 18:15:17 +0530

    REST methods PUT.POST,DELETE added

--- src/main/java/com/example/brewery/web/services/beerServiceImpl.java
+++ src/main/java/com/example/brewery/web/services/beerServiceImpl.java
@@ -1,13 +1,30 @@
 package com.example.brewery.web.services;
 
 import com.example.brewery.web.model.beerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class beerServiceImpl implements BeerService{
     @Override
     public beerDto getBeerById(UUID beerId) {
         return beerDto.builder().id(UUID.randomUUID()).beerName("galaxy cat").beerStyle("Pale Ale").build();
+    }
+
+    @Override
+    public beerDto saveBeerDto(beerDto savedDto) {
+        return beerDto.builder().id(UUID.randomUUID()).build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, beerDto beerDto) {
+        //TODO - ADD LOGIC
+    }
+
+    @Override
+    public void deleteBeerById(UUID beerId) {
+        log.debug("Deleting a beer with uuid" + beerId.toString());
     }
 }
--- src/main/java/com/example/brewery/web/services/customerServiceImpl.java
+++ src/main/java/com/example/brewery/web/services/customerServiceImpl.java
@@ -1,15 +1,32 @@
 package com.example.brewery.web.services;
 
 import com.example.brewery.web.model.customerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class customerServiceImpl implements CustomerService{
 
     @Override
     public customerDto getCustomerById(UUID id) {
         return customerDto.builder().id(id).customerName("Gary").build();
+    }
+
+    @Override
+    public customerDto addCustomer(customerDto newCustomer) {
+        return customerDto.builder().id(newCustomer.getId()).customerName(newCustomer.getCustomerName()).build();
+    }
+
+    @Override
+    public void updateCustomer(UUID custId, customerDto customerDto) {
+        //TODO
+    }
+
+    @Override
+    public void deleteCustomer(UUID custId) {
+        log.debug("Deleteing cutomer .." + custId);
     }
 }
